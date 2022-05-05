a5f23c13005de21a85da5948980b4f8f6a0cc78a
Slemmie <thorvejeneriksen@gmail.com>
2022-05-05 14:19:03 +0200

    add all/dark/light -squares constants in bit_board.h

--- src/game/bit_board.h
+++ src/game/bit_board.h
@@ -6,4 +6,15 @@ namespace game {
 	
 	typedef uint64_t bit_board;
 	
+	namespace bb {
+		
+		constexpr bit_board all_squares =
+		~static_cast <bit_board> (0);
+		constexpr bit_board dark_squares =
+		static_cast <bit_board> (0b1010101001010101101010100101010110101010010101011010101001010101);
+		constexpr bit_board light_squares =
+		static_cast <bit_board> (0b0101010110101010010101011010101001010101101010100101010110101010);
+		
+	} /// namespace bb
+	
 } /// namespace game
